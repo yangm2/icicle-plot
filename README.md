0b02e1351c99839b55e9ecc3fb664924851274fa
yangm2 <yangm2@users.noreply.github.com>
2025-07-01 12:11:31 -0700

    initialized with uv tooling basic dir structure and argparse code

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # icicle-plot
 produce plot of hierarchical data
+
+## Generate plot
+```sh
+% uv run src/main.py
+```
+
+## Development
+
+- install locked version of project dependencies and tools
+  ```sh
+  % uv sync
+  ```
+- format code
+  ```sh
+  % uv run ruff format
+  ```
+- lint code
+  ```sh
+  % uv run ruff check
+  ```
+- typecheck code
+  ```sh
+  % uv run {mypy|pyrefly|ty} [TYPECHECK OPTIONS]
+  ```
+- upgrade locked version of project dependencies and tools
+  ``` sh
+  % uv lock --upgrade
+  ```
+
---
+++ src/main.py
@@ -0,0 +1,33 @@
+import argparse
+from pathlib import Path
+import tomllib
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="icicle plot generator for hierarchical data"
+    )
+    parser.add_argument(
+        "--input",
+        type=lambda s: Path(s).resolve(strict=True),
+        required=True,
+        help="Path to the input TOML for the icicle plot",
+    )
+    parser.add_argument(
+        "--output",
+        type=lambda p: Path(p).resolve(),
+        required=True,
+        help="Path to save the output icicle plot",
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    _input_data = tomllib.loads(args.input.read_text())
+    _output_path: Path = args.output
+    print("Hello from icicle-plot!")
+
+
+if __name__ == "__main__":
+    main()
